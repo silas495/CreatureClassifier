e368450d45552c0eaded7e264344e8c38a9fca41
Silas Praat <silaspraat@gmail.com>
2020-08-27 15:50:08 +0200

    changed output layer to softmax 2

--- Classifier.py
+++ Classifier.py
@@ -27,7 +27,7 @@ def define_model():
 	model.add(Flatten())
 	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
 	model.add(Dropout(0.5))
-	model.add(Dense(1, activation='sigmoid'))
+	model.add(Dense(2, activation='softmax'))
 	# compile model
 	opt = SGD(lr=0.001, momentum=0.9)
 	model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
